63785963ab7e1c7d639662b2469c47acc80ea0da
tonyday567 <tonyday567@gmail.com>
2016-10-26 11:32:08 +1000

    laws expanded

--- test/test.hs
+++ test/test.hs
@@ -2,14 +2,14 @@
 
 module Main where
 
-import Protolude hiding ((+),(-),(*),(/),zero,one,negate,div,mod,rem,quot, Integral(..), Semiring(..), Bool(..),(<>), Semigroup, empty)
+import Protolude hiding ((+),(-),(*),(/),zero,one,negate)
 
 import Test.Tasty (TestName, TestTree, testGroup, defaultMain)
 import Test.Tasty.QuickCheck
 
 import Math.Gaia
 import Math.Gaia.Integer()
-import Math.Gaia.Bool
+import Math.Gaia.Bool()
 
 data LawArity a =
     Unary (a -> Bool) |
@@ -39,47 +39,29 @@ main = defaultMain tests
 
 laws ::
     ( Eq a
-    , Num a
     , Distributive a
+    , Invertible (Add a)
+    , Commutative (Add a)
+    , Lattice a
+    , Homomorphic (Inf a) (Sup a)
     ) =>
     [Law a]
 laws =
     [ ("associative: a + (b + c) == (a + b) + c", Ternary (\a b c -> a + (b + c) == (a + b) + c))
-    , ("left id: 0 + a = a", Unary (\a -> 0 + a == a))
-    , ("right id: a + 0 = a", Unary (\a -> a + 0 == a))
+    , ("left zero: zero + a = a", Unary (\a -> zero + a == a))
+    , ("right zero: a + zero = a", Unary (\a -> a + zero == a))
+    , ("left one: one * a == a", Unary (\a -> one * a == a))
+    , ("right one: a * one == a", Unary (\a -> a * one == a))
     , ("commutative: a + b == b + a", Binary (\a b -> a + b == b + a))
-    , ("associative: (a * b) * c == a * (b * c)", Ternary (\a b c -> (a * b) * c == a * (b * c)))
     , ("commutative: a * b == b * a", Binary (\a b -> a * b == b * a))
-    , ("left annihilative: a * 0 == 0", Unary (\a -> a * 0 == 0))
-    , ("right annihilative: 0 * a == 0", Unary (\a -> 0 * a == 0))
+    , ("associative: (a * b) * c == a * (b * c)", Ternary (\a b c -> (a * b) * c == a * (b * c)))
+    , ("left annihilative: a * zero == zero", Unary (\a -> a * zero == zero))
+    , ("right annihilative: zero * a == zero", Unary (\a -> zero * a == zero))
     , ("left distributive: a * (b + c) == a * b + a * c", Ternary (\a b c -> a * (b + c) == a * b + a * c))
     , ("right distributive: (a + b) * c == a * c + b * c", Ternary (\a b c -> (a + b) * c == a * c + b * c))
-    , ("left id: 1 * a == a", Unary (\a -> 1 * a == a))
-    , ("right id: a * 1 == a", Unary (\a -> a * 1 == a))
-    -- , ("group rightminus1: (a + b) - b = a", Binary (\a b -> (a + b) - b == a))
-    -- , ("group rightminus2: a + (b - b) = a", Binary (\a b -> a + (b - b) == a))
-    -- , ("group negateminus: a + negate b == a - b", Binary (\a b -> a + negate b == a - b))
-    -- , ("group leftinverse: negate a + a == zero", Unary (\a -> negate a + a == zero))
-    -- , ("group rightinverse: a + negate a == zero", Unary (\a -> a + negate a == zero))
-    ]
-
-laws' :: [Law Integer]
-laws' =
-    [ ("associative: a + (b + c) == (a + b) + c", Ternary (\a b c -> a + (b + c) == (a + b) + c))
-    , ("left id: 0 + a = a", Unary (\a -> 0 + a == a))
-    , ("right id: a + 0 = a", Unary (\a -> a + 0 == a))
-    , ("commutative: a + b == b + a", Binary (\a b -> a + b == b + a))
-    , ("associative: (a * b) * c == a * (b * c)", Ternary (\a b c -> (a * b) * c == a * (b * c)))
-    , ("commutative: a * b == b * a", Binary (\a b -> a * b == b * a))
-    , ("left annihilative: a * 0 == 0", Unary (\a -> a * 0 == 0))
-    , ("right annihilative: 0 * a == 0", Unary (\a -> 0 * a == 0))
-    , ("left distributive: a * (b + c) == a * b + a * c", Ternary (\a b c -> a * (b + c) == a * b + a * c))
-    , ("right distributive: (a + b) * c == a * c + b * c", Ternary (\a b c -> (a + b) * c == a * c + b * c))
-    , ("left id: 1 * a == a", Unary (\a -> 1 * a == a))
-    , ("right id: a * 1 == a", Unary (\a -> a * 1 == a))
-    -- , ("group rightminus1: (a + b) - b = a", Binary (\a b -> (a + b) - b == a))
-    -- , ("group rightminus2: a + (b - b) = a", Binary (\a b -> a + (b - b) == a))
-    -- , ("group negateminus: a + negate b == a - b", Binary (\a b -> a + negate b == a - b))
-    -- , ("group leftinverse: negate a + a == zero", Unary (\a -> negate a + a == zero))
-    -- , ("group rightinverse: a + negate a == zero", Unary (\a -> a + negate a == zero))
+    , ("right minus1: (a + b) - b = a", Binary (\a b -> (a + b) - b == a))
+    , ("right minus2: a + (b - b) = a", Binary (\a b -> a + (b - b) == a))
+    , ("negate minus: a + negate b == a - b", Binary (\a b -> a + negate b == a - b))
+    , ("left inverse: negate a + a == zero", Unary (\a -> negate a + a == zero))
+    , ("right inverse: a + negate a == zero", Unary (\a -> a + negate a == zero))
     ]
